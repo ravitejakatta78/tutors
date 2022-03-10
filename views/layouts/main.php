7672fedaddbf707786267d748b14f4c83f8fb254
ravitejakatta78 <ravitejakatta78@gmail.com>
2022-03-10 12:24:35 +0530

    Added banner related changes

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -17,7 +17,7 @@ AppAsset::register($this);
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>FOODQ Merchant</title>
+    <title>SuperPilot Merchant</title>
     <link rel="shortcut icon" href="<?php echo Url::base(); ?>/favicon.png" type="image/x-icon" />
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
