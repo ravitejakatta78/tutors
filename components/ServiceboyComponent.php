e860c8f3af119407367208a3a7c7f65148b5ed82
ravitejakatta78 <ravitejakatta78@gmail.com>
2022-03-19 22:19:25 +0530

    Changed prepare label

--- components/ServiceboyComponent.php
+++ components/ServiceboyComponent.php
@@ -670,7 +670,7 @@ class ServiceboyComponent extends Component{
 						$orderarray['paidstatus'] =  $orderlist['paidstatus']; 
 						$orderarray['orderline'] =  $orderlist['orderline']; 
 						$orderarray['reorder'] =  $orderlist['reorderprocess']; 
-						$orderarray['preparetime'] = $orderlist['preparetime']; 
+						$orderarray['preparetime'] = date('Y-m-d h:i:s A',strtotime($orderlist['preparetime'])); 
 						$orderarray['updatedtime'] = $orderlist['mod_date'];
 						$orderarray['preparedate'] = $orderlist['preparedate'];
 						$orderarray['merchant_id'] = $orderlist['merchant_id'];
@@ -779,7 +779,7 @@ class ServiceboyComponent extends Component{
 			$orderarray['reorder'] =  $orderlist['reorderprocess'];
 			$orderarray['reg_date'] =  date('d-M-Y h:i A',strtotime($orderlist['reg_date']) ); 
 			$orderarray['preparedate'] =  $orderlist['preparedate'];
-			$orderarray['preparetime'] = $orderlist['preparetime']; 
+			$orderarray['preparetime'] = date('Y-m-d h:i:s A',strtotime($orderlist['preparetime'])); 
 			$orderarray['orderline'] =  $orderlist['orderline'];
 			$orderarray['instructions'] =  $orderlist['instructions'] ?? '';
 			$orderarray['discount_number'] =  sprintf("%.2f", (!empty($orderlist['discount_number']) ?   $orderlist['discount_number'] : 0));
